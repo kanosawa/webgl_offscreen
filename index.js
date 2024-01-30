b340869d421807491ac1061566226216834d7eb9
kanosawa <kanokanosawasawa@gmail.com>
2024-01-31 01:16:49 +0900

    OffscreenクラスにrenderOffscreenTexture関数を追加。

--- index.js
+++ index.js
@@ -42,6 +42,44 @@ class Offscreen {
         return this.#framebuffer;
     }
 
+    renderOffscreenTexture() {
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.vertices), this.gl.STATIC_DRAW);
+    
+        // 頂点属性の設定
+        const positionAttribLocation = this.gl.getAttribLocation(this.shaderProgram, 'aVertexPosition');
+        this.gl.enableVertexAttribArray(positionAttribLocation);
+        this.gl.vertexAttribPointer(positionAttribLocation, 2, this.gl.FLOAT, false, 0, 0);
+    
+        // テクスチャ座標の設定
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureCoordBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.textureCoordinates), this.gl.STATIC_DRAW);
+    
+        // テクスチャ座標属性の設定
+        const textureCoordAttribLocation = this.gl.getAttribLocation(this.shaderProgram, 'aTextureCoord');
+        this.gl.enableVertexAttribArray(textureCoordAttribLocation);
+        this.gl.vertexAttribPointer(textureCoordAttribLocation, 2, this.gl.FLOAT, false, 0, 0);
+    
+        // テクスチャのバインド
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+    
+        // シェーダプログラムの使用
+        this.gl.useProgram(this.shaderProgram);
+    
+        // テクスチャサンプラーの設定
+        this.gl.uniform1i(this.gl.getUniformLocation(this.shaderProgram, 'uSampler'), 0);
+    
+        // メインキャンバスにテクスチャを描画
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+        this.gl.clearColor(0.0, 0.0, 0.0, 0.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+        this.gl.viewport(0, 0, this.width, this.height);
+        this.gl.enable(this.gl.BLEND);
+        this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+    }
+
     constructor(gl) {
         this.gl = gl;
         this.texture = gl.createTexture();
@@ -51,13 +89,55 @@ class Offscreen {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         this.#framebuffer = gl.createFramebuffer();
+
+        // テクスチャ用の頂点シェーダ
+        const vertexShaderSource =
+        'attribute vec4 aVertexPosition;' +
+        'attribute vec2 aTextureCoord;' +
+        'varying highp vec2 vTextureCoord;' +
+        'void main() {' +
+        '    gl_Position = aVertexPosition;' +
+        '    vTextureCoord = aTextureCoord;' +
+        '}';
+
+        // テクスチャ用のフラグメントシェーダ
+        const fragmentShaderSource =
+        'precision mediump float;' +
+        'varying highp vec2 vTextureCoord;' +
+        'uniform sampler2D uSampler;' +
+        'void main() {' +
+        '    gl_FragColor = texture2D(uSampler, vTextureCoord);' +
+        '    gl_FragColor.a = 0.3;' +
+        '}';
+
+        // シェーダプログラムの作成
+        this.shaderProgram = createShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
+        this.vertexBuffer = gl.createBuffer();
+        this.textureCoordBuffer = gl.createBuffer();
     }
 
     gl;
     #framebuffer;
     texture;
+    shaderProgram;
+    vertexBuffer;
+    textureCoordBuffer;
     width;
     height;
+
+    vertices = [
+        -1, -1,
+        1, -1,
+        -1,  1,
+        1,  1,
+    ];
+
+    textureCoordinates = [
+        0.0,  0.0,
+        1.0,  0.0,
+        0.0,  1.0,
+        1.0,  1.0,
+    ];
 };
 
 // オフスクリーンレンダリング
@@ -81,7 +161,7 @@ function offscreenRendering(gl, offscreen) {
     const shaderProgram = createShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
 
     // 頂点情報の設定
-    const vertexBuffer = gl.createBuffer();
+    vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     const vertices = [
         -1, -1,
@@ -105,84 +185,6 @@ function offscreenRendering(gl, offscreen) {
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(shaderProgram);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-    return offscreen.texture;
-}
-
-// オフスクリーンテクスチャの描画
-function renderOffscreenTexture(gl, canvas, offscreenTexture) {
-
-    // テクスチャ用の頂点シェーダ
-    const vertexShaderSource =
-    'attribute vec4 aVertexPosition;' +
-    'attribute vec2 aTextureCoord;' +
-    'varying highp vec2 vTextureCoord;' +
-    'void main() {' +
-    '    gl_Position = aVertexPosition;' +
-    '    vTextureCoord = aTextureCoord;' +
-    '}';
-
-    // テクスチャ用のフラグメントシェーダ
-    const fragmentShaderSource =
-    'precision mediump float;' +
-    'varying highp vec2 vTextureCoord;' +
-    'uniform sampler2D uSampler;' +
-    'void main() {' +
-    '    gl_FragColor = texture2D(uSampler, vTextureCoord);' +
-    '    gl_FragColor.a = 0.3;' +
-    '}';
-
-    // シェーダプログラムの作成
-    const shaderProgram = createShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
-
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    const vertices = [
-        -0.5, -0.5,
-        0.5, -0.5,
-        -0.5,  0.5,
-        0.5,  0.5,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    // 頂点属性の設定
-    const positionAttribLocation = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
-    gl.enableVertexAttribArray(positionAttribLocation);
-    gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
-
-    // テクスチャ座標の設定
-    const textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-    const textureCoordinates = [
-        0.0,  0.0,
-        1.0,  0.0,
-        0.0,  1.0,
-        1.0,  1.0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
-
-    // テクスチャ座標属性の設定
-    const textureCoordAttribLocation = gl.getAttribLocation(shaderProgram, 'aTextureCoord');
-    gl.enableVertexAttribArray(textureCoordAttribLocation);
-    gl.vertexAttribPointer(textureCoordAttribLocation, 2, gl.FLOAT, false, 0, 0);
-
-    // テクスチャのバインド
-    gl.bindTexture(gl.TEXTURE_2D, offscreenTexture);
-
-    // シェーダプログラムの使用
-    gl.useProgram(shaderProgram);
-
-    // テクスチャサンプラーの設定
-    gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uSampler'), 0);
-
-    // メインキャンバスにテクスチャを描画
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
 function init() {
@@ -197,6 +199,6 @@ function init() {
     }
 
     const offscreen = new Offscreen(gl);
-    const offscreenTexture = offscreenRendering(gl, offscreen);
-    renderOffscreenTexture(gl, canvas, offscreenTexture);
+    offscreenRendering(gl, offscreen);
+    offscreen.renderOffscreenTexture();
 }
